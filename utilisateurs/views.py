ea719aa316eaaccc7630c992784304e18657f7d7
zineb <126409044+catmilkomg@users.noreply.github.com>
2023-05-03 22:38:19 +0100

    push

--- utilisateurs/views.py
+++ utilisateurs/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 def acceuil(request):
-    return render(request, 'utilisateurs/index.html')
+    return render(request, 'utilisateurs/login.html')
 
 
 def register(request):
